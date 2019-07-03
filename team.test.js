7cc06b9dc65df2c39242b9e82ddf05dc0ba92ca9
cindychea <cindy_chea@hotmail.com>
2019-07-03 00:05:54 -0400

    completed testing reinforcement exercise: wrote test to check that function returns object

--- team.test.js
+++ team.test.js
@@ -1,5 +1,11 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('returns object with correct structure', () => {
+  // arrange
+  const testName = 'Unikron';
+  const testLevel = 5;
+  // act
+  const result = teamObj(testName, testLevel);
+  //assert
+  expect(result).toEqual({'teamName': testName, 'level': testLevel, 'points': 0})
+});
